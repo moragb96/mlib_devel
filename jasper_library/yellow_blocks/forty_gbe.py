36091a506a9946f98db4b33d086107334f2513cd
AdamI75 <aisaacson@ska.ac.za>
2020-01-15 14:39:51 +0200

    Update 40GbE QSFP pblock constraint The 40GbE pblock constraint did not include all the PHY logic - some of the PHY logic was being routed to the MEZ0 HMC pblock causing the placer to fail due to too many clocks being routed in the X0Y7 device region, which can only handle 12 global clocks. The constraint has now been fixed.

--- jasper_library/yellow_blocks/forty_gbe.py
+++ jasper_library/yellow_blocks/forty_gbe.py
@@ -929,8 +929,7 @@ class forty_gbe(YellowBlock):
 
 
         cons.append(RawConstraint('create_pblock MEZ3_'+self.mez3_phy+'_QSFP'))
-        cons.append(RawConstraint('add_cells_to_pblock [get_pblocks MEZ3_'+self.mez3_phy+'_QSFP] [get_cells -quiet [list '+self.fullname+'/IEEE802_3_XL_PHY_'+str(self.port)+'/PHY_inst/RX_CLK_RCC]]'))
-        cons.append(RawConstraint('add_cells_to_pblock [get_pblocks MEZ3_'+self.mez3_phy+'_QSFP] [get_cells -quiet [list '+self.fullname+'/IEEE802_3_XL_PHY_'+str(self.port)+'/PHY_inst/TX_CLK_RCC]]'))
+        cons.append(RawConstraint('add_cells_to_pblock [get_pblocks MEZ3_'+self.mez3_phy+'_QSFP] [get_cells -quiet [list '+self.fullname+'/IEEE802_3_XL_PHY_'+str(self.port)+'/PHY_inst]]'))
         cons.append(RawConstraint('resize_pblock [get_pblocks MEZ3_'+self.mez3_phy+'_QSFP] -add {'+self.clock_region+'}'))
 
         #Raw Constraints
